6d462836b8e8a9c03b2c9a595364d01e107ef035
ibraimovsasha <ibraimovsasha@gmail.com>
2021-07-04 15:35:46 +0600

    Added Differentiate method

--- MathematicalAnalysis/Diff/MathExtensions.cs
+++ MathematicalAnalysis/Diff/MathExtensions.cs
@@ -1,7 +1,21 @@
-﻿namespace Diff
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Diff
 {
     public static class MathExtensions
     {
+        private static readonly Dictionary<ExpressionType, Func<Expression, Expression>> differentiatedFuncs;
 
+        /// <summary>
+        /// Function differentiation
+        /// </summary>
+        /// <param name="function">Function to be differentiated</param>
+        /// <returns>Differentiated function</returns>
+        public static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> function)
+        {
+            return Expression.Lambda<Func<double, double>>(differentiatedFuncs[function.Body.NodeType](function.Body), function.Parameters);
+        }
     }
 }
