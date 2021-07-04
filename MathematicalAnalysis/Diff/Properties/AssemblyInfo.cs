d956bef65458f47a1123a1175526659dd7089617
ibraimovsasha <ibraimovsasha@gmail.com>
2021-07-04 17:01:01 +0600

    Added publish to nuget

--- MathematicalAnalysis/Diff/Properties/AssemblyInfo.cs
+++ MathematicalAnalysis/Diff/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Diff")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Extensions over Math")]
+[assembly: AssemblyDescription("Extensions over Math. Allows differentiation of functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ibrai")]
 [assembly: AssemblyProduct("Diff")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Not used by NuGet")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
