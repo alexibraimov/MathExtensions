d5a1437b3d892f2e812562054d3d1319d8deab06
ibraimovsasha <ibraimovsasha@gmail.com>
2021-07-04 15:47:58 +0600

    Added basic functions

--- MathematicalAnalysis/Diff/MathExtensions.cs
+++ MathematicalAnalysis/Diff/MathExtensions.cs
@@ -25,6 +25,16 @@ namespace Diff
             {
                 [nameof(Math.Sin)] = e => DiffSin(e),
                 [nameof(Math.Cos)] = e => DifCos(e),
+                [nameof(Math.Pow)] = e => DiffPow(e),
+                [nameof(Math.Exp)] = e => DiffExp(e),
+                [nameof(Math.Log)] = e => DiffLog(e),
+                [nameof(Math.Tan)] = e => DiffTan(e),
+                [nameof(Math.Asin)] = e => DiffAsin(e),
+                [nameof(Math.Acos)] = e => DiffAcos(e),
+                [nameof(Math.Atan)] = e => DiffAtan(e),
+                [nameof(Math.Tanh)] = e => DiffTanh(e),
+                [nameof(Math.Sinh)] = e => DiffSinh(e),
+                [nameof(Math.Cosh)] = e => DiffCosh(e),
             };
 
             derivativesFuncs = new Dictionary<ExpressionType, Func<Expression, Expression>>();
@@ -54,7 +64,7 @@ namespace Diff
             });
         }
 
-        static Expression DiffSin(MethodCallExpression e)
+        private static Expression DiffSin(MethodCallExpression e)
         {
             return Expression.Multiply(
                         Expression.Call(null, typeof(Math).GetMethod(nameof(Math.Cos)), e.Arguments[0]),
@@ -62,12 +72,62 @@ namespace Diff
                         );
         }
 
-        static Expression DifCos(MethodCallExpression e)
+        private static Expression DifCos(MethodCallExpression e)
         {
             return Expression.Multiply(
                        Expression.Multiply(Expression.Call(null, typeof(Math).GetMethod(nameof(Math.Sin)), e.Arguments[0]),
                        Expression.Constant(-1.0)),
                        derivativesFuncs[e.Arguments[0].NodeType](e.Arguments[0]));
+        }
+
+        private static Expression DiffCosh(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffSinh(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffTanh(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffAtan(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffAcos(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffAsin(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffTan(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffLog(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffExp(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression DiffPow(MethodCallExpression e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
